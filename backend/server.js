59db2a157728231ccb723467f7c89441e01b4305
Prateek Ranjan <prateekranjan766@gmail.com>
2021-03-19 01:06:50 +0530

    Preparing to deploy

--- backend/server.js
+++ backend/server.js
@@ -29,6 +29,16 @@ app.get('/api/config/paypal', (req, res) =>
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'frontend', 'build')));
+
+  app.use('*', express.static(path.join(__dirname, 'frontend', 'build')));
+} else {
+  app.get('/', (req, res) => {
+    res.send(`Api running...`);
+  });
+}
+
 app.use(notFound);
 app.use(errorHandler);
 
